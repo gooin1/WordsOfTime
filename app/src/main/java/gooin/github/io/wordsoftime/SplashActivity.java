bcd44e715924de84ceec0862a8e6153e70c36169
gooin <gooin@outlook.com>
2016-06-22 23:37:23 +0800

    添加第一屏

---
+++ app/src/main/java/gooin/github/io/wordsoftime/SplashActivity.java
@@ -0,0 +1,30 @@
+package gooin.github.io.wordsoftime;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        finish();
+                    }
+                },300);
+            }
+        },500);
+    }
+}
